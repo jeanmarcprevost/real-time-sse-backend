be842ccac0d5c60882ba8ad3b588e2c52cbf17d8
Jean-Marc <jeanmarc.prevost@gmail.com>
2018-09-21 11:55:24 +0200

    [ADD] simulation server

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+  console.log('Requested url: ' + request.url);
+
+  if (request.url.toLowerCase() === '/events') {
+    response.writeHead(200, {
+      'Connection': 'keep-alive',
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      'Access-Control-Allow-Origin': '*'
+    });
+
+    setTimeout(() => {
+      response.write(
+        'data: {"flight": "I768", "state": "landing"}'
+      );
+      response.write('\n\n');
+    }, 3000);
+
+    setTimeout(() => {
+      response.write(
+        'data: {"flight": "I768", "state": "landed"}'
+      );
+      response.write('\n\n');
+    }, 6000);
+
+    setTimeout(() => {
+      response.write(
+        'data: {"flight": "I768", "state": "coucou"}'
+      );
+      response.write('\n\n');
+    }, 9000);
+
+    setTimeout(() => {
+      response.write(
+        'data: {"flight": "I999", "origin": "Strabsourg", "arrival": "15:15", "state": ""}'
+      );
+      response.write('\n\n');
+    }, 12000);
+
+  } else {
+    response.writeHead(404);
+    response.end();
+  }
+}).listen(5000, () => {
+  console.log('Server running at http://127.0.0.1:5000/');
+});
